6f309cdbfc97d3154be2dc651acb5a5a25393730
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-10-24 20:24:01 +0300

    HW 53 (104) done after webinar

--- java-oop-algorithm/src/telran/strings/JoinStringsPerformanceAppl.java
+++ java-oop-algorithm/src/telran/strings/JoinStringsPerformanceAppl.java
@@ -12,41 +12,31 @@ public class JoinStringsPerformanceAppl {
 	private static final String BASE_PACKAGE = "telran.strings.";
 
 	@SuppressWarnings("unchecked")
-	public static void main(String[] args) throws Exception {
-		String[] strings = getStrings();
-	//	String testNameString = getTestName("JoinStringsImpl");
-	//	String testNameStringBuilder = getTestName("JoinStringsBuilderImpl");
+	public static void main(String[] args)  {
+		
 		if(args.length < 1) {
 			System.out.println("Too few arguments: must be name of class");
 		} else {
+			String[] strings = getStrings();
+					
+				for(String className : args) {
 			try {
-				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + args[0]);
+				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + className);
 				Constructor<JoinStrings> constructor = clazz.getConstructor();
 				JoinStrings joinStrings = constructor.newInstance();
-				String testNameString = getTestName(args[0]);
+				String testNameString = getTestName(className);
 				JoinStringsPerformanceTest testString = new JoinStringsPerformanceTest(testNameString, N_RUNS, strings,
 						joinStrings);
 				testString.run();
 			} catch (ClassNotFoundException e) {
-				System.out.println("Wrong className:  " + args[0]);
+				System.out.println("Wrong className:  " + className);
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
-			
+			}
 		}
 	
-		//JoinStringsImpl jsi = new JoinStringsImpl();
-		//JoinStringsBuilderImpl jsbi = new JoinStringsBuilderImpl();
-	/*
-		JoinStringsPerformanceTest testStringBuilder =
-				new JoinStringsPerformanceTest(testNameStringBuilder, N_RUNS,
-strings, jsbi);
-		JoinStringsPerformanceTest testString =
-				new JoinStringsPerformanceTest(testNameString, N_RUNS,
-strings, jsi);
-		testStringBuilder.run();
-		testString.run();
-		*/
+	
 	}
 
 	private static String getTestName(String className) {
